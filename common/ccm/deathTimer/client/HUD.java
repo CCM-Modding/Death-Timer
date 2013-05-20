5ce4240807ce9a648a7a08972da64ced9fce20d1
Dries007 <admin@dries007.net>
2013-05-20 03:21:16 +0200

    Now with F3 seperation for clear visibility

--- common/ccm/deathTimer/client/HUD.java
+++ common/ccm/deathTimer/client/HUD.java
@@ -17,10 +17,13 @@ public class HUD
     @ForgeSubscribe
     public void render(Text event)
     {
+        boolean b = (event.left.size() != 0);
         if (ClientDeathTimer.enable)
         {
+            if (b) event.left.add("----------------");
             event.left.add("T- item despawn:");
             event.left.add(FunctionHelper.timeColor(ClientDeathTimer.time) + FunctionHelper.parseTime(ClientDeathTimer.time));
+            if (b) event.left.add("----------------");
         }
     }
 }
