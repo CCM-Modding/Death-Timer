37323a92ddb05292848ce25f3f2034aa1c644a73
Captain Shadows <captain_shadows@aol.com>
2013-06-12 00:21:36 -0400

    Stuff!!

--- common/ccm/deathTimer/commands/Timer.java
+++ common/ccm/deathTimer/commands/Timer.java
@@ -7,240 +7,243 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumChatFormatting;
+
 import ccm.deathTimer.api.HardTimerAPI;
 import ccm.deathTimer.server.ServerTimer;
 import ccm.deathTimer.timerTypes.DeathTimer;
 import ccm.deathTimer.timerTypes.ITimerBase;
 import ccm.deathTimer.timerTypes.PointTimer;
-import ccm.nucleum_omnium.helper.JavaHelper;
+import ccm.nucleum.helper.JavaHelper;
 
 /**
- * You need OP to change server wide settings, Cient only changes are send back via packets and are saved in the client config.
+ * You need OP to change server wide settings, Cient only changes are send back via packets and are
+ * saved in the client config.
  * 
  * @author Dries007
  */
 public class Timer extends CommandBase
 {
+
     public final static String ListSyntax  = "list";
-    
+
     public final static String AddSyntax   = "add <name> <time> [X] [Y] [Z] [dim]";
-    
+
     public final static String StopSyntax  = "stop <name>";
-    
+
     public final static String SoundSyntax = "sound <name> [volume] [pitch]";
-    
+
     public final static String PointSyntax = "point <name> [X] [Y] [Z] [dim]";
-    
+
     public final static String OpArgs      = "[" + Timer.ListSyntax + "|" + Timer.AddSyntax + "|" + Timer.StopSyntax + "|" + Timer.SoundSyntax + "|" + Timer.PointSyntax + "]";
-    
+
     public final static String NonOpArgs   = "[" + Timer.ListSyntax + "|" + Timer.StopSyntax + "|" + Timer.SoundSyntax + "]";
-    
+
     @Override
     public String getCommandName()
     {
         return "timer";
     }
-    
+
     /**
      * Main command thing
      */
     @Override
     public void processCommand(final ICommandSender sender, final String[] args)
     {
-        final boolean op = sender instanceof EntityPlayer ? MinecraftServer.getServer().isDedicatedServer() ? MinecraftServer.getServer().getConfigurationManager().getOps().contains(sender.getCommandSenderName()) : true : true;
-        
-        if (args.length == 1 && args[0].equalsIgnoreCase("help"))
-        {
+        final boolean op = sender instanceof EntityPlayer ? MinecraftServer.getServer().isDedicatedServer() ? MinecraftServer.getServer()
+                .getConfigurationManager()
+                .getOps()
+                .contains(sender.getCommandSenderName()) : true : true;
+
+        if ((args.length == 1) && args[0].equalsIgnoreCase("help")){
             sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Useage: /" + this.getCommandName() + " " + (op ? Timer.OpArgs : Timer.NonOpArgs));
-            if (!op) sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Note that you can only change your own timers!");
+            if (!op){
+                sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Note that you can only change your own timers!");
+            }
             return;
         }
-        
-        if (args.length == 0 || args[0].equalsIgnoreCase("timers") || args[0].equalsIgnoreCase("list"))
-        {
+
+        if ((args.length == 0) || args[0].equalsIgnoreCase("timers") || args[0].equalsIgnoreCase("list")){
             sender.sendChatToPlayer("--- Timers ---");
-            for (final ITimerBase timer : ServerTimer.getInstance().timerList.values())
-            {
+            for (final ITimerBase timer : ServerTimer.getInstance().timerList.values()){
                 final StringBuilder sb = new StringBuilder();
-                for (final String s : timer.getTimerString(sender))
+                for (final String s : timer.getTimerString(sender)){
                     sb.append(EnumChatFormatting.RESET.toString() + EnumChatFormatting.WHITE.toString() + s);
+                }
                 sender.sendChatToPlayer(sb.toString());
             }
             sender.sendChatToPlayer("--------------");
-        }
-        else if (op && args[0].equalsIgnoreCase("add"))
+        }else if (op && args[0].equalsIgnoreCase("add")){
             this.processCommandAdd(sender, args, op);
-        else if (op && args[0].equalsIgnoreCase("point"))
+        }else if (op && args[0].equalsIgnoreCase("point")){
             this.processCommandPoint(sender, args, op);
-        else if (args[0].equalsIgnoreCase("stop"))
+        }else if (args[0].equalsIgnoreCase("stop")){
             this.processCommandStop(sender, args, op);
-        else if (args[0].equalsIgnoreCase("sound"))
+        }else if (args[0].equalsIgnoreCase("sound")){
             this.processCommandSound(sender, args, op);
-        else
-        {
+        }else{
             sender.sendChatToPlayer(EnumChatFormatting.RED + "Syntax error. Use: /" + this.getCommandName() + " " + (op ? Timer.OpArgs : Timer.NonOpArgs));
-            if (!op) sender.sendChatToPlayer(EnumChatFormatting.RED + "Note that you can only change your own timers!");
+            if (!op){
+                sender.sendChatToPlayer(EnumChatFormatting.RED + "Note that you can only change your own timers!");
+            }
             return;
         }
     }
-    
+
     /**
      * Stop part
      */
     public void processCommandStop(final ICommandSender sender, final String[] args, final boolean op)
     {
-        if (args.length < 1)
-        {
-            if (ServerTimer.getInstance().timerList.containsKey(DeathTimer.PREFIX + sender.getCommandSenderName()))
+        if (args.length < 1){
+            if (ServerTimer.getInstance().timerList.containsKey(DeathTimer.PREFIX + sender.getCommandSenderName())){
                 HardTimerAPI.stopTimer(DeathTimer.PREFIX + sender.getCommandSenderName());
-            else
+            }else{
                 sender.sendChatToPlayer(EnumChatFormatting.RED + "Syntax error. Use: /" + this.getCommandName() + " " + Timer.StopSyntax);
-        }
-        else if (ServerTimer.getInstance().timerList.containsKey(args[1]))
-        {
+            }
+        }else if (ServerTimer.getInstance().timerList.containsKey(args[1])){
             final ITimerBase timer = ServerTimer.getInstance().timerList.get(args[1]);
-            
-            if (timer.isRelevantFor(sender) && timer.isPersonal() || op)
-            {
+
+            if ((timer.isRelevantFor(sender) && timer.isPersonal()) || op){
                 HardTimerAPI.stopTimer(args[1]);
                 sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Done.");
-            }
-            else
+            }else{
                 sender.sendChatToPlayer(EnumChatFormatting.RED + "You can't edit this timer.");
-        }
-        else
+            }
+        }else{
             sender.sendChatToPlayer(EnumChatFormatting.RED + "This timer doesn't exists.");
+        }
     }
-    
+
     /**
      * Sound part
      */
     public void processCommandSound(final ICommandSender sender, final String[] args, final boolean op)
     {
-        if (args.length < 1)
+        if (args.length < 1){
             sender.sendChatToPlayer(EnumChatFormatting.RED + "Syntax error. Use: /" + this.getCommandName() + " " + Timer.SoundSyntax);
-        else if (ServerTimer.getInstance().timerList.containsKey(args[1]))
-        {
+        }else if (ServerTimer.getInstance().timerList.containsKey(args[1])){
             final ITimerBase timer = ServerTimer.getInstance().timerList.get(args[1]);
-            
-            if (args.length < 5)
-            {
-                if (timer.isRelevantFor(sender) && timer.isPersonal() || op)
-                {
-                    if (JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[3]))
-                    {
+
+            if (args.length < 5){
+                if ((timer.isRelevantFor(sender) && timer.isPersonal()) || op){
+                    if (JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[3])){
                         HardTimerAPI.setSound(args[1], args[2], Float.valueOf(args[3]), Float.valueOf(args[4]));
                         sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Done.");
-                    }
-                    else if (!JavaHelper.isNumeric(args[3]))
+                    }else if (!JavaHelper.isNumeric(args[3])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[3] + " is not a number!");
-                    else if (!JavaHelper.isNumeric(args[4]))
+                    }else if (!JavaHelper.isNumeric(args[4])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[4] + " is not a number!");
-                    else
+                    }else{
                         sender.sendChatToPlayer("?????????? < Please report to modmaker, idk how you got here...");
-                }
-                else
+                    }
+                }else{
                     sender.sendChatToPlayer(EnumChatFormatting.RED + "You can't edit this timer.");
-            }
-            else
+                }
+            }else{
                 sender.sendChatToPlayer("Current sound: " + timer.getSoundName() + " Volume:" + timer.getSoundVolume() + " Pitch: " + timer.getSoundPitch());
-        }
-        else
+            }
+        }else{
             sender.sendChatToPlayer(EnumChatFormatting.RED + "This timer doesn't exists.");
+        }
     }
-    
+
     /**
      * Point part
      */
     public void processCommandPoint(final ICommandSender sender, final String[] args, final boolean op)
     {
-        if (args.length < 1)
+        if (args.length < 1){
             sender.sendChatToPlayer(EnumChatFormatting.RED + "Syntax error. Use: /" + this.getCommandName() + " " + Timer.PointSyntax);
-        else if (ServerTimer.getInstance().timerList.containsKey(args[1]) && ServerTimer.getInstance().timerList.get(args[1]) instanceof PointTimer)
-        {
+        }else if (ServerTimer.getInstance().timerList.containsKey(args[1]) && (ServerTimer.getInstance().timerList.get(args[1]) instanceof PointTimer)){
             final PointTimer timer = (PointTimer) ServerTimer.getInstance().timerList.get(args[1]);
-            
-            if (args.length < 5)
-            {
-                if (timer.isRelevantFor(sender) && timer.isPersonal() || op)
-                {
-                    if (JavaHelper.isNumeric(args[2]) || JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[4]))
-                    {
+
+            if (args.length < 5){
+                if ((timer.isRelevantFor(sender) && timer.isPersonal()) || op){
+                    if (JavaHelper.isNumeric(args[2]) || JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[4])){
                         timer.X = Integer.parseInt(args[2]);
                         timer.Y = Integer.parseInt(args[3]);
                         timer.Z = Integer.parseInt(args[4]);
-                        if (args.length == 6 && JavaHelper.isNumeric(args[5])) timer.dim = Integer.parseInt(args[5]);
+                        if ((args.length == 6) && JavaHelper.isNumeric(args[5])){
+                            timer.dim = Integer.parseInt(args[5]);
+                        }
                         sender.sendChatToPlayer(EnumChatFormatting.GREEN + "Done.");
                         timer.sendAutoUpdate();
-                    }
-                    else if (!JavaHelper.isNumeric(args[3]))
+                    }else if (!JavaHelper.isNumeric(args[3])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[3] + " is not a number!");
-                    else if (!JavaHelper.isNumeric(args[4]))
+                    }else if (!JavaHelper.isNumeric(args[4])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[4] + " is not a number!");
-                    else if (!JavaHelper.isNumeric(args[5]))
+                    }else if (!JavaHelper.isNumeric(args[5])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[5] + " is not a number!");
-                    else if (args.length < 5 && !JavaHelper.isNumeric(args[6]))
+                    }else if ((args.length < 5) && !JavaHelper.isNumeric(args[6])){
                         sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[6] + " is not a number!");
-                    else
+                    }else{
                         sender.sendChatToPlayer("?????????? < Please report to modmaker, idk how you got here...");
-                }
-                else
+                    }
+                }else{
                     sender.sendChatToPlayer(EnumChatFormatting.RED + "You can't edit this timer.");
-            }
-            else
+                }
+            }else{
                 sender.sendChatToPlayer("Current point: [" + timer.X + ";" + timer.Y + ";" + timer.Z + "] in world " + timer.dim);
-        }
-        else
+            }
+        }else{
             sender.sendChatToPlayer(EnumChatFormatting.RED + "This timer doesn't exists or is not a point timer.");
+        }
     }
-    
+
     /**
      * Add part
      */
     public void processCommandAdd(final ICommandSender sender, final String[] args, final boolean op)
     {
-        if (args.length < 3)
+        if (args.length < 3){
             sender.sendChatToPlayer(EnumChatFormatting.RED + "Syntax error. Use: /" + this.getCommandName() + " " + Timer.AddSyntax);
-        else if (ServerTimer.getInstance().timerList.containsKey(args[1]))
+        }else if (ServerTimer.getInstance().timerList.containsKey(args[1])){
             sender.sendChatToPlayer(EnumChatFormatting.RED + "This timer already exists.");
-        else if (!JavaHelper.isNumeric(args[2]))
+        }else if (!JavaHelper.isNumeric(args[2])){
             sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[2] + " is not a number!");
-        else if (args.length == 6 || args.length == 7)
-        {
-            if (JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[4]) || JavaHelper.isNumeric(args[5]))
-            {
-                if (!(sender instanceof EntityPlayer) && args.length == 6)
+        }else if ((args.length == 6) || (args.length == 7)){
+            if (JavaHelper.isNumeric(args[3]) || JavaHelper.isNumeric(args[4]) || JavaHelper.isNumeric(args[5])){
+                if (!(sender instanceof EntityPlayer) && (args.length == 6)){
                     sender.sendChatToPlayer("You need to specify a dimention!");
-                else if (args.length == 7 && !JavaHelper.isNumeric(args[6]))
+                }else if ((args.length == 7) && !JavaHelper.isNumeric(args[6])){
                     sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[2] + " is not a number!");
-                else
-                    HardTimerAPI.newPointTimer(args[1], Integer.valueOf(args[2]), Integer.valueOf(args[3]), Integer.valueOf(args[4]), Integer.valueOf(args[5]), args.length == 7 ? Integer.valueOf(args[6]) : ((EntityPlayer) sender).dimension);
-            }
-            else if (!JavaHelper.isNumeric(args[3]))
+                }else{
+                    HardTimerAPI.newPointTimer(args[1],
+                                               Integer.valueOf(args[2]),
+                                               Integer.valueOf(args[3]),
+                                               Integer.valueOf(args[4]),
+                                               Integer.valueOf(args[5]),
+                                               args.length == 7 ? Integer.valueOf(args[6]) : ((EntityPlayer) sender).dimension);
+                }
+            }else if (!JavaHelper.isNumeric(args[3])){
                 sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[3] + " is not a number!");
-            else if (!JavaHelper.isNumeric(args[4]))
+            }else if (!JavaHelper.isNumeric(args[4])){
                 sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[4] + " is not a number!");
-            else if (!JavaHelper.isNumeric(args[5]))
+            }else if (!JavaHelper.isNumeric(args[5])){
                 sender.sendChatToPlayer(EnumChatFormatting.RED + " " + args[5] + " is not a number!");
-            else
+            }else{
                 sender.sendChatToPlayer("?????????? < Please report to modmaker, idk how you got here...");
-        }
-        else
+            }
+        }else{
             HardTimerAPI.newBasicTimer(args[1], Integer.valueOf(args[2]));
+        }
     }
-    
+
     @Override
-    @SuppressWarnings({ "unchecked" })
+    @SuppressWarnings(
+    { "unchecked" })
     public List<String> addTabCompletionOptions(final ICommandSender sender, final String[] args)
     {
-        if (args.length == 1)
+        if (args.length == 1){
             return CommandBase.getListOfStringsMatchingLastWord(args, "help", "list", "add", "stop", "sound", "point");
-        else if (args.length == 2 && (args[0].equalsIgnoreCase("stop") || args[0].equalsIgnoreCase("sound") || args[0].equalsIgnoreCase("stop")))
+        }else if ((args.length == 2) && (args[0].equalsIgnoreCase("stop") || args[0].equalsIgnoreCase("sound") || args[0].equalsIgnoreCase("stop"))){
             return CommandBase.getListOfStringsFromIterableMatchingLastWord(args, ServerTimer.getInstance().timerList.keySet());
-        else
+        }else{
             return null;
+        }
     }
-    
+
     @Override
     public boolean canCommandSenderUseCommand(final ICommandSender par1ICommandSender)
     {
