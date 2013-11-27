0b0c6754dfa68469ed389d09def4ec4129857e36
Captain Shadows <captain_shadows@aol.com>
2013-11-27 18:31:30 -0500

    Updated to ForgeGradle

--- src/main/java/ccm/deathTimer/utils/lib/Archive.java
+++
@@ -1,9 +0,0 @@
-package ccm.deathTimer.utils.lib;
-
-public final class Archive
-{
-    public static final String MOD_ID = "deathTimer";
-    public static final String MOD_NAME = "Death Timer";
-    public static final String MOD_CHANNEL_TIMERS = "DT";
-    public static final String MOD_DEPENDANCIES = "required-after:nucleum_omnium";
-}
--- src/main/java/ccm/deathTimer/utils/lib/Locations.java
+++
@@ -1,8 +0,0 @@
-package ccm.deathTimer.utils.lib;
-
-public final class Locations
-{
-    public static final String proxy = "ccm." + Archive.MOD_ID + ".proxy.";
-    public static final String CLIENT_PROXY = Locations.proxy + "ClientProxy";
-    public static final String SERVER_PROXY = Locations.proxy + "CommonProxy";
-}
